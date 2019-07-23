e19bc3ee71c72ac31055b127b9ae8db546abba72
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-07-23 08:46:22 +0700

    Model type to reflect game state

---
+++ src/core/game.ts
@@ -0,0 +1,29 @@
+export type Game = {
+    state: 'IDLE'
+} | {
+    state: 'PLAYING'
+    activePlayer: Player
+    opponent: Player
+} | {
+    state: 'END'
+    winner: Player
+    loser: Player
+}
+
+export type Player = {
+    health: number
+    // - start: 30
+    mana: ManaSlot[]
+    // - start: 0 max 10
+    deck: Card[]
+    // - start: 20 damage cards, mana costs: 0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8
+    cards: Card[]
+    // - start: 3 random cards
+}
+
+export type ManaSlot = 'EMPTY' | 'FILLED'
+
+export type Card = {
+    damage: number
+    manaCost: number
+}
