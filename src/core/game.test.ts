826f04154ede33f4e4a3b518cad2023101929499
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-07-24 05:16:52 +0700

    Preparation: initial deck
    * Each player starts with a deck of 20 Damage cards
    with the following Mana costs:
    0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8

--- src/core/game.test.ts
+++ src/core/game.test.ts
@@ -1,4 +1,4 @@
-import { start } from "./game";
+import { start, Card } from "./game";
 
 describe('Game', () => {
     describe('preparation', () => {
@@ -11,6 +11,18 @@ describe('Game', () => {
 
                 expect(game.opponent.health).toBe(30)
                 expect(game.opponent.mana).toEqual([])
+            } else {
+                fail(`state must be 'PLAYING' but got ${game.state}`)
+            }
+        })
+
+        test('each player starts with a deck of 20 Damage cards with the following Mana costs: 0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8', () => {
+            if (game.state === 'PLAYING') {
+                const initialDeckManaCosts = [0, 0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 7, 8]
+                const deckManaCosts = (deck: Card[]) => deck.map(card => card.manaCost).sort()
+
+                expect(deckManaCosts(game.activePlayer.deck)).toEqual(initialDeckManaCosts)
+                expect(deckManaCosts(game.opponent.deck)).toEqual(initialDeckManaCosts)
             } else {
                 fail(`state must be 'PLAYING' but got ${game.state}`)
             }
