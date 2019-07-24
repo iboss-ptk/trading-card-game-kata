5cccfc033aa4c7ab88433d64c131df447af4c863
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-07-24 07:57:39 +0700

    Preparation: Initial hand
    * From the deck each player receives 3 random cards has his initial hand

--- src/core/game.test.ts
+++ src/core/game.test.ts
@@ -1,8 +1,9 @@
-import { start, Card, initialDraw } from "./game";
+import { start, Card, initialDraw, Player } from "./game";
 
 describe('Game', () => {
     describe('preparation', () => {
         const game = start()
+        const initialDeckManaCosts = [0, 0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 7, 8]
 
         test('each player starts the game with 30 Health and 0 Mana slots', () => {
             expect(game.activePlayer.health).toBe(30)
@@ -13,12 +14,26 @@ describe('Game', () => {
         })
 
         test('each player starts with a deck of 20 Damage cards with the following Mana costs: 0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8', () => {
-            const initialDeckManaCosts = [0, 0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 7, 8]
             const deckManaCosts = (deck: Card[]) => deck.map(card => card.manaCost).sort()
 
             expect(deckManaCosts(game.activePlayer.deck)).toEqual(initialDeckManaCosts)
             expect(deckManaCosts(game.opponent.deck)).toEqual(initialDeckManaCosts)
         })
 
+        test('from the deck each player receives 3 random cards has his initial hand', () => {
+            const initialDrawed = initialDraw(game)
+
+            const combineHandAndDeck = (player: Player) =>
+                player.deck
+                    .concat(player.hand)
+                    .map(card => card.manaCost)
+                    .sort()
+
+            expect(initialDrawed.activePlayer.hand.length).toBe(3)
+            expect(combineHandAndDeck(initialDrawed.activePlayer)).toEqual(initialDeckManaCosts)
+
+            expect(initialDrawed.opponent.hand.length).toBe(3)
+            expect(combineHandAndDeck(initialDrawed.opponent)).toEqual(initialDeckManaCosts)
+        })
     })
 })
--- src/core/game.ts
+++ src/core/game.ts
@@ -1,3 +1,6 @@
+import R from 'ramda'
+import shuffle from 'lodash/shuffle'
+
 export type Game = {
     activePlayer: Player
     opponent: Player
@@ -10,7 +13,7 @@ export type Player = {
     // - start: 0 max 10
     deck: Card[]
     // - start: 20 damage cards, mana costs: 0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8
-    cards: Card[]
+    hand: Card[]
     // - start: 3 random cards
 }
 
@@ -27,10 +30,46 @@ const newPlayer = (): Player => ({
     health: 30,
     mana: [],
     deck: initialDeck,
-    cards: []
+    hand: []
 })
 
 export const start = (): Game => ({
     activePlayer: newPlayer(),
     opponent: newPlayer()
 })
+
+const draw = (count: number, deck: Card[]) =>
+    R.take(count, shuffle(deck))
+
+const subtractList = <T>(a: T[], b: T[]) => {
+    let _a: T[] = R.clone(a)
+    let _b: T[] = R.clone(b)
+
+    _b.forEach(e => {
+        const i = R.indexOf(e, _a)
+        if (i !== -1) {
+            _a.splice(i, 1)
+        }
+    })
+
+    return _a
+}
+
+const initialPlayerDraw = (player: Player) => {
+    const drawed = draw(3, player.deck)
+    return R.mergeRight(
+        player,
+        {
+            deck: subtractList(player.deck, drawed),
+            hand: drawed
+        }
+    )
+}
+
+export const initialDraw = (game: Game): Game => R.mergeRight(
+    game,
+    {
+        activePlayer: initialPlayerDraw(game.activePlayer),
+        opponent: initialPlayerDraw(game.opponent)
+    }
+)
